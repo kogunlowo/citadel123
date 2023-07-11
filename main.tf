14c30b00eaa7d625de6279cb8928830d6f730a61
Citadel Cloud Management <138833672+CitadelCloudManagement@users.noreply.github.com>
2023-07-11 18:55:17 -0500

    Updated repo123

--- main.tf
+++ main.tf
@@ -31,13 +31,3 @@ resource "azurerm_storage_account" "storeacc" {
     change_feed_enabled      = var.change_feed_enabled
   }
 
-  dynamic "network_rules" {
-    for_each = var.network_rules != null ? ["true"] : []
-    content {
-      default_action             = "Deny"
-      bypass                     = var.network_rules.bypass
-      ip_rules                   = var.network_rules.ip_rules
-      virtual_network_subnet_ids = var.network_rules.subnet_ids
-    }
-  }
-}
