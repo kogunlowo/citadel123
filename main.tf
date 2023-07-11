60a2ffbe95f12ae97dc57689d15101bd2b8f25d2
Citadel Cloud Management <138833672+CitadelCloudManagement@users.noreply.github.com>
2023-07-11 18:49:58 -0500

    Updated repo

---
+++ main.tf
@@ -0,0 +1,43 @@
+
+
+resource "azurerm_storage_account" "storeacc" {
+  name                      = substr(format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result), 0, 24)
+  resource_group_name       = local.resource_group_name
+  location                  = local.location
+  account_kind              = var.account_kind
+  account_tier              = local.account_tier
+  account_replication_type  = local.account_replication_type
+  enable_https_traffic_only = true
+  min_tls_version           = var.min_tls_version
+  tags                      = merge({ "ResourceName" = substr(format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result), 0, 24) }, var.tags, )
+
+  dynamic "identity" {
+    for_each = var.managed_identity_type != null ? [1] : []
+    content {
+      type         = var.managed_identity_type
+      identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? var.managed_identity_ids : null
+    }
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = var.blob_soft_delete_retention_days
+    }
+    container_delete_retention_policy {
+      days = var.container_soft_delete_retention_days
+    }
+    versioning_enabled       = var.enable_versioning
+    last_access_time_enabled = var.last_access_time_enabled
+    change_feed_enabled      = var.change_feed_enabled
+  }
+
+  dynamic "network_rules" {
+    for_each = var.network_rules != null ? ["true"] : []
+    content {
+      default_action             = "Deny"
+      bypass                     = var.network_rules.bypass
+      ip_rules                   = var.network_rules.ip_rules
+      virtual_network_subnet_ids = var.network_rules.subnet_ids
+    }
+  }
+}
